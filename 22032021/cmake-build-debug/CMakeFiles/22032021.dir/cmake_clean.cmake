f08204d932b0aa7c027e4c2bb2d1499110ab0b89
kleim <kleimenov.mi@phystech.edu>
2021-03-22 09:27:21 +0300

    ex. a

---
+++ 22032021/cmake-build-debug/CMakeFiles/22032021.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "22032021.exe"
+  "22032021.exe.manifest"
+  "22032021.pdb"
+  "CMakeFiles/22032021.dir/a.cpp.obj"
+  "lib22032021.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/22032021.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
